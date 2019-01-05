2e323db2073b09bb863ff5398182cab79f456652
Fede Otaran <otaran.federico@gmail.com>
2019-01-05 13:15:54 -0300

    DB simple configuration

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 from flask import Flask
 from flask import render_template
+from os import path
+from db import init_db
 
-app = Flask(__name__)
+app = Flask(__name__, instance_relative_config=True)
+app.config.from_mapping(
+    SECRET_KEY='dev',
+    DATABASE=path.join('db.sqlite'),
+)
 
 @app.route("/")
 def hello():
@@ -13,5 +19,11 @@ def issues():
     return render_template('issues/index.html')
 
 
+@app.cli.command('initdb')
+def initdb_command():
+    init_db()
+    print("Vamooooo")
+
+
 if __name__ == "__main__":
-  app.run(debug=True)
+    app.run(debug=True)
---
+++ schema.sql
@@ -0,0 +1,23 @@
+DROP TABLE IF EXISTS issues;
+DROP TABLE IF EXISTS categories;
+DROP TABLE IF EXISTS statuses;
+
+CREATE TABLE issues (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  email TEXT UNIQUE NOT NULL,
+  description TEXT NOT NULL,
+  category_id INTEGER NOT NULL,
+  status_id INTEGER NOT NULL,
+  FOREIGN KEY (category_id) REFERENCES categories (id),
+  FOREIGN KEY (status_id) REFERENCES statuses (id)
+);
+
+CREATE TABLE categories (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT NOT NULL
+);
+
+CREATE TABLE statuses (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT NOT NULL
+);
